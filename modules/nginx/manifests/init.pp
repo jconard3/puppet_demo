84360036185b2ea2900512211d673ba4a091456f
Jordan Conard <jconard@RSG-2585-Jordan-Conard.local>
2017-02-14 14:18:37 -0500

    Created simple html file for default page. Created file resource placing html at default nginx location on server.

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -2,6 +2,15 @@ class nginx {
   package { "nginx":
     ensure => installed,
   }->
+  file { "default":
+    path => '/usr/share/nginx/html/index.html',
+    ensure => file,
+    source => 'puppet:///modules/nginx/hello_world.html',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    notify => Service['nginx'],
+  }->
   service { "nginx":
     ensure => running,
   }
