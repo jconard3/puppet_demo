5c84ed5db1d7044782684ee874192e8344b64299
Jordan Conard <jconard@RSG-2585-Jordan-Conard.local>
2017-02-15 09:56:54 -0500

    Added tree package.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo yum -y install epel-release
 sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-sudo yum -y install puppet git
+sudo yum -y install puppet git tree
